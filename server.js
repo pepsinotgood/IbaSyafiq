0f533b813fcd8105c058d7da943ff45de15e4b67
pepsinotgood <adiba.geeks@gmail.com>
2024-08-27 02:08:06 +0800

    deploy be22

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Get the MongoDB URI from the environment variable or use localhost for development
-const MONGODB_URI = process.env.MONGODB_URI;
-
-// Connect to MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
